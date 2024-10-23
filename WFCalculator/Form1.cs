39d67cd32dcf7fdec2a731e0a31d7e10b21f4d31
Nikola Markovi─ç <nikola.markovic@student.etf.unibl.org>
2024-10-23 18:53:33 +0200

    refactor (bracket convention)

--- WFCalculator/Form1.cs
+++ WFCalculator/Form1.cs
@@ -6,14 +6,12 @@ namespace WFCalculator
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
         }
 
         // on load (valjda)
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
             textBox_main.Text = "";
         }
 
@@ -31,54 +29,49 @@ namespace WFCalculator
         private void button_backspace_Click(object sender, EventArgs e) => InputPop(textBox_main, 1);
 
         // '='
-        private void button_equals_Click(object sender, EventArgs e)
-        {
+        private void button_equals_Click(object sender, EventArgs e) {
 
         }
 
         // main/input display
-        private void textBox_main_TextChanged(object sender, EventArgs e)
-        {
+        private void textBox_main_TextChanged(object sender, EventArgs e) {
 
         }
 
         // result
-        private void textBox_res_TextChanged(object sender, EventArgs e)
-        {
+        private void textBox_res_TextChanged(object sender, EventArgs e) {
 
         }
 
         // quick mode checkbox
-        private void checkBox_qm_CheckedChanged(object sender, EventArgs e)
-        {
+        private void checkBox_qm_CheckedChanged(object sender, EventArgs e) {
 
         }
 
         const string sEmptyStringException = " received an empty string.";
         const string sNonNaturalNumberException = " expected a natural number.";
-        private void RawInput(TextBox target, string value)
-        {
+        private void RawInput(TextBox target, string value) {
             const string SelfName = "RawInput";
-            if (!string.IsNullOrEmpty(value))
-            {
+            if (!string.IsNullOrEmpty(value)) {
                 target.Text += value;
             }
             else throw new Exception(SelfName + sEmptyStringException);
         }
 
-        private void InputPop(TextBox target, int i)
-        {
+        private void InputPop(TextBox target, int i) {
             const string SelfName = "InputPop";
             // caller error, throw exception
-            if (i >= 1)
-            {
-                // user error, no need for an exception
-                if (!string.IsNullOrEmpty(target.Text))
-                {
+            if (i >= 1) {
+                // user "error", no need for an exception
+                if (!string.IsNullOrEmpty(target.Text)) { 
                     target.Text = target.Text.Remove(target.Text.Length - i);
                 }
             }
             else throw new Exception(SelfName + sNonNaturalNumberException);
+        }
+
+        private void InputReset(TextBox target) {
+
         }
     }
 }
